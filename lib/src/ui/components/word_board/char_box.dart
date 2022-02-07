3f17e5a026904a1a55a18e3b67cd5b56524d2ba9
Trí Nguyễn <torrydo.mobile@gmail.com>
2022-02-07 11:18:55 +0700

    bug: streamController not work, gridview still not updated

--- lib/src/ui/components/word_board/char_box.dart
+++ lib/src/ui/components/word_board/char_box.dart
@@ -1,48 +1,44 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class CharBox extends StatefulWidget {
+
+  final int index;
+
   final double boxSize;
   final String character;
-
   final double borderSize;
   final Color backgroundColor;
   final Color borderColor;
   final double borderRadius;
   final double fontSize;
 
-  final int index;
-
   final Function(int index)? onClick;
 
-  const CharBox(
-      {Key? key,
-      required this.boxSize,
-      required this.character,
-      this.index = -1,
-      this.borderSize = 2.5,
-      this.backgroundColor = Colors.transparent,
-      this.borderColor = Colors.deepOrangeAccent,
-      this.borderRadius = 7.0,
-      this.fontSize = 40.0,
-      this.onClick})
-      : super(key: key);
+  const CharBox({
+    Key? key,
+    required this.boxSize,
+    required this.character,
+    this.index = -1,
+    this.borderSize = 2.5,
+    this.backgroundColor = Colors.transparent,
+    this.borderColor = Colors.deepOrangeAccent,
+    this.borderRadius = 7.0,
+    this.fontSize = 40.0,
+    this.onClick
+    }) : super(key: key);
 
   @override
   _CharBoxState createState() => _CharBoxState();
 }
 
 class _CharBoxState extends State<CharBox> {
+
   @override
   Widget build(BuildContext context) {
-    _character = widget.character;
-
     return _charBox(widget.boxSize);
   }
-
-  var _character = '';
 
   Widget _charBox(
     double boxSize,
@@ -63,7 +59,7 @@ class _CharBoxState extends State<CharBox> {
                     BorderRadius.all(Radius.circular(widget.borderRadius))),
             child: Center(
               child: Text(
-                _character,
+                widget.character,
                 style: TextStyle(
                     fontSize: widget.fontSize, fontWeight: fontWeight),
               ),
@@ -79,10 +75,7 @@ class _CharBoxState extends State<CharBox> {
     if (widget.onClick != null) {
       widget.onClick!(widget.index);
     }
-    setState(() {
-      _character = 'X';
-    });
 
-    log("_char = $_character");
+    log("widget.character = ${widget.character}");
   }
 }
--- lib/src/ui/game_screen.dart
+++ lib/src/ui/game_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:wordle_game/src/ui/components/word_box/char_box.dart';
-import 'package:wordle_game/src/ui/components/word_box/word_grid_view.dart';
+import 'package:wordle_game/src/ui/components/word_board/word_grid_view.dart';
 import 'package:wordle_game/src/utils/get_width_height.dart';
 
 class GameScreen extends StatefulWidget {
@@ -25,7 +24,7 @@ class _GameScreenState extends State<GameScreen> {
         color: Colors.black87,
         child: Center(child: Padding(
           padding: EdgeInsets.symmetric(horizontal: marginHorizontal),
-          child: WordGridView(charNumber: 5, width: wordGridViewHeight),
+          child: WordGridView(wordLength: 5, width: wordGridViewHeight),
         )),
       ),
     );
